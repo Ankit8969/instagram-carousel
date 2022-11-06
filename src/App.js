db3f57206726483ec96e5165a0cb42bbe9fbe683
Ankit Kumar <47697582+Ankit8969@users.noreply.github.com>
2022-11-06 19:16:41 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Carousel from './components/Carousel';
+import "./styles/home.css";
+
+const App = () => {
+  return (
+    <section className='carousel-status'>
+      <Carousel />
+    </section>
+  )
+}
+
+export default App;
